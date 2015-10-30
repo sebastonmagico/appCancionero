589d9e885719241a9f7088209389625961c8d58a
Sebastian Tobon <sebastonmagico@yahoo.es>
2015-10-30 14:26:18 +0100

    Ma cagontó. Que no me sale la home con el versículo diario.

--- www/js/controllers/home_controller.js
+++ www/js/controllers/home_controller.js
@@ -1,4 +1,4 @@
-songbookApp.controller('HomeCtrl', function($scope, utils, newBook, $state, $timeout) {
+songbookApp.controller('HomeCtrl', function($scope, utils, newBook, $state, $timeout, curl) {
 
     $scope.init = function(){
       $scope.setted = false;
@@ -7,12 +7,24 @@ songbookApp.controller('HomeCtrl', function($scope, utils, newBook, $state, $tim
           $scope.randomSong = data.data[0];
           $scope.randomClass = 'homeBack' + utils.getRandom(1,10);
           $scope.setted = true;
-        }, 1000);
+        }, 500);
       })
+
     };
 
     $scope.goToSong = function(songID){
       $state.go('app.newbookdetail', {id: songID});
+    };
+
+    $scope.reloadSlider = function(){
+      $scope.setted = false;
+      newBook.findByID(utils.getRandom(1,233)).then(function(data){
+        $timeout(function(){
+          $scope.randomSong = data.data[0];
+          $scope.randomClass = 'homeBack' + utils.getRandom(1,10);
+          $scope.setted = true;
+        }, 500);
+      })
     };
 
 
---
+++ www/js/services/curl.js
@@ -0,0 +1,10 @@
+songbookApp.factory('curl', ['$window', '$http', function($window, $http) {
+
+  var url = "https://es.bibles.org/v2/versions.js";
+
+  return {
+    getVerse: function(){
+      return false;
+    }
+  }
+}]);
