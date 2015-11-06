09d52f7482a6ee02c62c72cd12b8e8f35759eeb3
Sebastian Tobon <sebastonmagico@yahoo.es>
2015-11-06 16:52:04 +0100

    Ou yeah madafakas. Por fin tengo el versículo aleatorio!!!, aunque de momento es un salmo y ya.

--- www/js/controllers/home_controller.js
+++ www/js/controllers/home_controller.js
@@ -1,18 +1,19 @@
-songbookApp.controller('HomeCtrl', function($scope, utils, newBook, $state, $timeout, curl) {
+songbookApp.controller('HomeCtrl', function($scope, utils, newBook, $state, $timeout) {
 
     $scope.init = function(){
       $scope.setted = false;
+      $scope.verseSetted = false;
       newBook.findByID(utils.getRandom(1,233)).then(function(data){
         $timeout(function(){
           $scope.randomSong = data.data[0];
           $scope.randomClass = 'homeBack' + utils.getRandom(1,10);
           $scope.setted = true;
+          utils.getVerse().then(function(data){
+            $scope.verseSetted = true;
+            $scope.verse = data.data;
+          });
         }, 500);
       })
-
-      curl.getVerse().then(function(data){
-        debugger;
-      });
 
     };
 
