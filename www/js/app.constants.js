9054e06db54a1762a5d6f71e0365e8f0ae111162
Tobon Vera Sebastian <sebastonmagico@yahoo.es>
2016-09-04 16:32:43 +0200

    Cambios en constantes.

--- www/js/app.constants.js
+++ www/js/app.constants.js
@@ -1,4 +1,4 @@
 angular.module('customConstants', []).constant("CONSTANTS", {
-        "SONGBOOK_INITIAL_LIMIT": 219,
+        "SONGBOOK_INITIAL_LIMIT": 1,
         "SONGBOOK_FINAL_LIMIT": 219
     });
